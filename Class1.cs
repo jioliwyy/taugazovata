bb941e0f71524d958b2b541181cfb8e0f9381977
taugazova <is24taugazovata@artcollege.ru>
2023-07-05 09:56:36 +0300

    класс Склад

--- Class1.cs
+++ Class1.cs
@@ -1,7 +1,29 @@
-﻿namespace taugazovata
-{
-    public class Class1
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taugazova31is
+{//Таугазова Т.А. 31ИС
+    // класс - склад
+    public class Store
     {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public List<Product> Products = new List<Product>();
+        public Store(string nm, string ad)
+        {
+            Name = nm;
+            Address = ad;
+        }
+        public void ShowAll()
+        {
+            Console.WriteLine($"Человек: имя - {Name}, адрес - {Address}");
+            Console.WriteLine("Список товаров:");
+            Product.Sort();
+            foreach (Product product in Products) { product.Show(); }
+        }
 
     }
 }
