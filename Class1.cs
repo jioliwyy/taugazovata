17ba7a7792ffdd9330d5e88094d4f72d318fd4d4
taugazova <is24taugazovata@artcollege.ru>
2023-07-05 10:04:05 +0300

    создала класс Товар

--- Class1.cs
+++ Class1.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace taugazovata
 {//Таугазова Т.А. 31ИС
-    // класс - склад
+    // класс склад
     public class Store
     {
         public string Name { get; set; }
